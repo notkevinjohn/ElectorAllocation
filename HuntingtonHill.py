9166f05e04140841fd0ec32c22b0ddb3fa71ca97
Kevin John <notkevinjohn@gmail.com>
2020-09-06 12:54:36 -0700

    updating HH method

--- HuntingtonHill.py
+++ HuntingtonHill.py
@@ -1,29 +1,44 @@
 #!/usr/bin/python3
 import math
+import time
 
 class HuntingtonHill():
+
+	initialElectors = 3
+
 	def selectState(self):
 		selectedState = None
 		highestQ = 0
-		for state in self.States:
-			stateCurrent = self.States[state]
-			Q = self.quotient(stateCurrent)
+		for key in self.StateData:
+			state = self.StateData[key]
+			Q = self.quotient(state)
 			if Q > highestQ:
 				highestQ = Q
-				selectedState = stateCurrent
+				selectedState = state
 		return selectedState
 
 	def quotient(self, state):
-		return state['Population'] / math.sqrt(state['Seats'] * ( state['Seats'] + 1))
+		return int(state['population']) / math.sqrt(state['electors'] * ( state['electors'] + 1))
 
-	def allocate(self, Seats, States):
-		self.Seats = Seats
-		self.States = States
-		while self.Seats > 0:
+	def allocate(self, Electors, StateData):
+		self.Electors = int(Electors)
+		self.StateData = StateData
+		print (self.Electors)
+
+		#Assign Initial Electors
+		for key in self.StateData:
+			state = self.StateData[key]
+			state['electors'] = self.initialElectors
+			self.Electors -= self.initialElectors
+
+		print (self.Electors)
+
+		#Assign population based Electors
+		while self.Electors > 0:
 			state = self.selectState()
-			state['Seats'] += 1
-			self.Seats -= 1
-			if self.Seats == 0:
-				break
+			state['electors'] += 1
+			self.Electors -= 1
+			print (state['abbreviation'], state['electors'])
+			time.sleep(1)
 
 
