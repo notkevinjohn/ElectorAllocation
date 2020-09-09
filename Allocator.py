91fc7b4e3ef18720803fef0ac1267ce1010f1d32
Kevin John <notkevinjohn@gmail.com>
2020-09-08 21:04:37 -0700

    removed config files and made electors a command line argument

--- Allocator.py
+++ Allocator.py
@@ -4,22 +4,23 @@ from HuntingtonHill import HuntingtonHill
 from HuntingtonHill_NoSenators import HuntingtonHill_NoSenators
 from HuntingtonHill_OOP import HuntingtonHill_OOP
 from DirectDemocracy import DirectDemocracy
+from DirectDemocracy_Senators import DirectDemocracy_Senators
 from DemocracyQuotient import DemocracyQuotient
 from Validator import Validator
 
 
 class Allocator():
-	def __init__(self, allocation, config, data):
+	def __init__(self, allocation, electors, data):
 		self.allocation = allocation
-		self.config = config
+		self.electors = electors
 		self.data = data
 
 	def allocate(self):
 		exec("self.allocator = "+self.allocation+"()")
-		self.allocator.allocate(self.config['Electors'], self.data)
+		self.allocator.allocate(self.electors, self.data)
 		validator = Validator()
 
-		if not validator.validate(self.config['Electors'], self.data):
+		if not validator.validate(self.electors, self.data):
 			raise Exception("Correct Number of Electors Not Allocated")
 
 		dq = DemocracyQuotient().calc(self.data)
--- Election.py
+++ Election.py
@@ -1,31 +1,35 @@
 import sys
 from Allocator import Allocator
 from Awarder import Awarder
-from ConfigLoader import ConfigLoader
+#from ConfigLoader import ConfigLoader
 from CSVLoader import CSVLoader
 import time
 
 class Election():
-	def __init__(self, allocation,awarding):
+	def __init__(self, allocation, awarding, electors):
 		self.allocation = allocation
 		self.awarding = awarding
+		self.electors = electors
 
-		configLoader = ConfigLoader("Config.xml")
-		self.config = configLoader.config
+		#configLoader = ConfigLoader("Config.xml")
+		#self.config = configLoader.config
 
 		csvLoader = CSVLoader("ElectionData.csv")
 		self.data = csvLoader.data
 
 	def elect(self):
-		allocator = Allocator(self.allocation, self.config, self.data)
+		allocator = Allocator(self.allocation, self.electors, self.data)
 		data, dq = allocator.allocate()
 
-		awarder = Awarder(self.awarding, self.config, self.data)
+		awarder = Awarder(self.awarding, self.electors, self.data)
 		votes_r, votes_d  = awarder.award()
-		print ('R:'+str(votes_r), 'D:'+str(votes_d))
+		#for key in data:
+		#	print (key, data[key]['electors'])
+		print ('R:'+str(votes_r), 'D:'+str(votes_d), dq)
 
 if __name__=="__main__":
 	allocation = sys.argv[1].split(".")[0]
 	awarding = sys.argv[2].split(".")[0]
-	election = Election(allocation, awarding)
+	electors = sys.argv[3]
+	election = Election(allocation, awarding, electors)
 	election.elect()
